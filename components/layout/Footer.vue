684566e1c778f30fb6c189e4723232c71e4bb7e2
Yannick <musayann@octangrp.com>
2020-04-17 20:27:42 +0200

    Add footer to the layout

---
+++ components/layout/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="panel bg-primary-dark h-auto min-h-auto">
+    <div class="text-center text-white  xs:text-xs text-sm font-medium">
+      &copy; 2016 - {{ year }} Octan Group &middot; All rights reserved &middot;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  computed: {
+    year() {
+      return '2020'
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,15 @@
     <div class="xl:block lg:block flex relative h-100 w-100">
       <nuxt />
     </div>
+    <Footer />
   </div>
 </template>
+
+<script>
+import Footer from '~/components/layout/Footer'
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
