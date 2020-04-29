e026f9b8b018ac4e5c867644819fa007e2879191
Yannick <musayann@octangrp.com>
2020-04-29 14:02:19 +0200

    Setup purgecss
    Installed purgecss to remove unused css classes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+import path from 'path'
+import PurgecssPlugin from 'purgecss-webpack-plugin'
+import glob from 'glob-all'
+
 export default {
   mode: 'universal',
   /*
@@ -85,6 +89,20 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extractCSS: true,
+    extend(config, { isDev, isClient }) {
+      if (!isDev && isClient) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            whitelist: ['html', 'body']
+          })
+        )
+      }
+    }
   }
 }
